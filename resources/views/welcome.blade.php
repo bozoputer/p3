1ccfadbe3b3c3abf125ecda5d8fae03a9d64ec0e
ebo-one <e@ebo.one>
2016-03-28 00:32:17 -0400

    Update page title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage Test</title>
+    <title>E Boone | Project 3 | CSCI E-15</title>
     <link rel="stylesheet" href="css/app.css">
     <link rel="stylesheet" href="css/styles.css">
 </head>
